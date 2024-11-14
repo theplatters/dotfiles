cf6d9eea02df51e85ada82d27034d11c3e7ea1b7
theplatters <fscharnreitner@gmail.com>
2024-11-14 10:49:04 +0100

    opam env

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,4 @@
 set -gx FZF_DEFAULT_COMMAND 'fd . --hidden'
 set -gx EDITOR nvim
 oh-my-posh init --config https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/catppuccin_macchiato.omp.json | source
+eval (opam env)
